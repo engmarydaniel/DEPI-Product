6e25cde0af23bfdbcedfc946c48ee1429489c73c
engmarydaniel <eng.marydaniel@outlook.com>
2025-04-28 13:15:23 +0300

    Add files via upload

--- DEPI-Project-master/DataAccess/Repository/ProductRepository.cs
+++ DEPI-Project-master/DataAccess/Repository/ProductRepository.cs
@@ -1,38 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using DataAccess.DataBase;
+﻿using DataAccess.DataBase;
 using DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
-
 using Models;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace DataAccess.Repository
 {
-    public class ProductRepository : Repository<Product>/*,IProductRepository*/
+    public class ProductRepository : Repository<Product>, IProductRepository
     {
+        private readonly AppDbContext _context;
         public ProductRepository(AppDbContext context) : base(context)
         {
+            _context = context;
         }
-
-        //public Task CreateAsync(Product entity)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
+        public IEnumerable<Product> SearchByName(string name)
+        {
+            return _context.Products
+                           .Where(p => p.Name.Contains(name))
+                           .ToList();
+        }
         public IEnumerable<Product> GetAll()
         {
-           
-            return dbSet?.Include(p => p.Category).AsEnumerable();
-
+            return _context.Products.ToList();
+        }
+        public Product GetById(int id)
+        {
+            return _context.Products
+                    .FirstOrDefault(p => p.Id == id);
         }
 
-        //Task<List<Product>> IRepository<Product>.GetAllAsync(Expression<Func<Product, bool>>? filter, string? includeProperties, int pageSize, int pageNumber)
-        //{
-        //    throw new NotImplementedException();
-        //}
+
+        public void Add(Product product)
+        {
+            _context.Products.Add(product);
+            
+        }
+
+        public void Update(Product product)
+        {
+            _context.Products.Update(product);
+            
+        }
+
+        public void Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                
+            }
+        }
     }
 }
