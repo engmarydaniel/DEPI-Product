3e9a6ee6ecee2d701256b97240a5b34aee86282e
engmarydaniel <eng.marydaniel@outlook.com>
2025-04-28 13:15:48 +0300

    Add files via upload

--- DEPI-Project-master/DataAccess/Repository/IRepository/IProductRepository.cs
+++ DEPI-Project-master/DataAccess/Repository/IRepository/IProductRepository.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Models;
+
 
 namespace DataAccess.Repository.IRepository
 {
-    public interface IProductRepository : IRepository<Product>
+    public interface IProductRepository
     {
+        IEnumerable<Product> GetAll();
+        IEnumerable<Product> SearchByName(string name);
+        Product GetById(int id);
+        void Add(Product category);
+        void Update(Product product);
         
+        void Delete(int id);
     }
+    
 }
