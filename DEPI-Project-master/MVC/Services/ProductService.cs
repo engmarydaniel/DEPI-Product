6e7db78f43dcd079c75e512cc6ad1b2277220aae
engmarydaniel <eng.marydaniel@outlook.com>
2025-04-29 13:25:40 +0300

    Add files via upload

---
+++ DEPI-Project-master/MVC/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Models;
+
+namespace MVC.Services
+{
+    public class ProductService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl = "http://localhost:5276/API"; // API
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Product>> GetProductsAsync()
+        {
+            var response = await _httpClient.GetAsync($"{_apiBaseUrl}/products");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+        }
+
+        public async Task<Product> GetProductWithReviewsAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"{_apiBaseUrl}/products/{id}/reviews");
+            response.EnsureSuccessStatusCode();
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Product>(json);
+        }
+    }
+}
