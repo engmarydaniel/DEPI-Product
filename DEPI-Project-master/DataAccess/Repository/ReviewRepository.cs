dc653b56f2225b6f19e16a64347e1009a7eea1c9
engmarydaniel <eng.marydaniel@outlook.com>
2025-04-29 09:03:35 +0300

    Add files via upload

---
+++ DEPI-Project-master/DataAccess/Repository/ReviewRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.DataBase;
+using Models;
+using DataAccess.Repository;
+using DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+
+namespace DataAccess.Repository
+{
+    public class ReviewRepository : Repository<Review>
+    {
+        private readonly AppDbContext _context;
+
+        public ReviewRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+       
+        public async Task<Review> GetReviewWithProductAsync(int productId)
+        {
+            return await _context.Reviews
+                .Include(r => r.Product)
+                //.ThenInclude(oi => oi.Product)
+                .FirstOrDefaultAsync(r => r.ProductId == productId);
+        }
+    }
+}
