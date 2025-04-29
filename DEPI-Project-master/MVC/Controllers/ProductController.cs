56be6e7e3db5f6c0a3fada5e97e65df06b7a46d2
engmarydaniel <eng.marydaniel@outlook.com>
2025-04-29 13:23:05 +0300

    Add files via upload

---
+++ DEPI-Project-master/MVC/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models.Dtos;
+using MVC.Services;
+using Newtonsoft.Json;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProductsAsync();
+            return View(products);
+        }
+
+        public async Task<IActionResult> Reviews(int id)
+        {
+            var product = await _productService.GetProductWithReviewsAsync(id);
+            return View(product);
+        }
+    }
+}
