7699840864fd27d93dd556ede18be09e271ec39a
engmarydaniel <eng.marydaniel@outlook.com>
2025-04-29 13:24:21 +0300

    Add files via upload

---
+++ DEPI-Project-master/MVC/Models/Review.cs
@@ -0,0 +1,39 @@
+﻿using Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace MVC.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        [Range(1, 5)]
+        [Required]
+        public int Rating { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        [JsonIgnore]
+        [IgnoreDataMember]
+        public AppUser User { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [JsonIgnore]
+        [IgnoreDataMember]
+        public Product Product { get; set; }
+    }
+}
