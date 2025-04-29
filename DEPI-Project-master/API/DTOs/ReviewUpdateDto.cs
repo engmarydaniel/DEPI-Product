034105f04b8ad70dc3616129cbf64fae9ae8f7df
engmarydaniel <eng.marydaniel@outlook.com>
2025-04-29 09:01:32 +0300

    Add files via upload

---
+++ DEPI-Project-master/API/DTOs/ReviewUpdateDto.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace API.DTOs
+{
+    public class ReviewUpdateDto
+    {
+        [Required]
+        [MaxLength(500)]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        [Range(1, 5)]
+        [Required]
+        public int Rating { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
